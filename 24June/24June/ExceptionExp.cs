7a804deffe40fa9cffa17cada2a6187eb25cc8e1
Vishrut Aryan <VAryan@holtecasia.com>
2024-06-25 12:45:08 +0530

    delegates

--- 24June/24June/ExceptionExp.cs
+++ 24June/24June/ExceptionExp.cs
@@ -20,11 +20,68 @@ namespace ExceptionExp
         }
     }
 
+    public delegate void dlg(Exception ex);
+
     public class bankacc
     {
         private int _accno;
         public string _accname;
         public double _balance;
+
+        public void print_error(Exception ex)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write((" || " + System.DateTime.Now.ToString()) + " || ");
+            Console.WriteLine();
+            Console.WriteLine(("    Error Message:   " + ex.Message));
+            Console.WriteLine();
+            Console.WriteLine(("    Error Type:      " + ex.GetType()));
+            Console.WriteLine();
+            Console.WriteLine(("    Source:          " + ex.Source));
+            Console.WriteLine();
+            Console.WriteLine(("    Stack Trace:  " + ex.StackTrace));
+            Console.WriteLine();
+            Console.WriteLine(("    Target Site:     " + ex.TargetSite));
+            Console.WriteLine();
+            Console.WriteLine(("    Inner Exception: " + ex.InnerException));
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Error Printed");
+        }
+
+        public void log_error(Exception ex)
+        {
+            FileStream fs = new FileStream(@"ErrorLog.pdf", FileMode.Append);
+            StreamWriter str = new StreamWriter(fs);
+
+            str.Write((" || " + System.DateTime.Now.ToString()) + " || ");
+            str.WriteLine();
+            str.WriteLine(("    Error Message:   " + ex.Message));
+            str.WriteLine();
+            str.WriteLine(("    Error Type:      " + ex.GetType()));
+            str.WriteLine();
+            str.WriteLine(("    Source:          " + ex.Source));
+            str.WriteLine();
+            str.WriteLine(("    Stack Trace:  " + ex.StackTrace));
+            str.WriteLine();
+            str.WriteLine(("    Target Site:     " + ex.TargetSite));
+            str.WriteLine();
+            str.WriteLine(("    Inner Exception: " + ex.InnerException));
+            str.WriteLine();
+            str.WriteLine();
+
+            str.Close();
+            fs.Close();
+
+            Console.WriteLine("Error Logged");
+        }
+
+        public void error_logging(dlg d, Exception ex)
+        {
+            d(ex);
+        }
 
         public bankacc()
         {
@@ -52,44 +109,9 @@ namespace ExceptionExp
             }
             catch (Exception ex) 
             {
-                Console.WriteLine();
-                Console.WriteLine();
-
-                string fileName = "ErrorLog.txt";
-                string priorText = File.ReadAllText(fileName);
-
-                using (StreamWriter writer = new StreamWriter(fileName))
-                {
-                    Console.Write((" || " + System.DateTime.Now.ToString()) + " || ");
-                    Console.WriteLine();
-                    Console.WriteLine(("    Error Message:   " + ex.Message));
-                    Console.WriteLine();
-                    Console.WriteLine(("    Error Type:      " + ex.GetType()));
-                    Console.WriteLine();
-                    Console.WriteLine(("    Source:          " + ex.Source));
-                    Console.WriteLine();
-                    Console.WriteLine(("    Stack Trace:  " + ex.StackTrace));
-                    Console.WriteLine();
-                    Console.WriteLine(("    Target Site:     " + ex.TargetSite));
-                    Console.WriteLine();
-                    Console.WriteLine(("    Inner Exception: " + ex.InnerException));
-                    Console.WriteLine();
-                    Console.WriteLine();
-                }
-                string readText = File.ReadAllText(fileName);
-                
-                using (StreamWriter writer = new StreamWriter(fileName))
-                {
-                    Console.WriteLine("Would you like to clear the file before writing? Enter Y if yes");
-                    if ("Y" != Console.ReadLine().ToUpper())
-                    {
-                        writer.WriteLine(priorText);
-                    }
-                    writer.WriteLine(readText);
-                }
-                string finalReadText = File.ReadAllText(fileName);
-
-                Console.WriteLine(finalReadText);
+                dlg d = new dlg(log_error);
+                d += new dlg(print_error);
+                error_logging(d, ex);
             }
         }
     }
--- 25June/25June/Program.cs
+++ 25June/25June/Program.cs
@@ -1,18 +1,61 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _25June
 {
-    public class DataStore<T>
+    public class DataStore<T> // Class as Strongly Typed Collection
     {
         public T record_code;
         public T record_uid;
         public DateTime CreationDate;
     }
+
+    public static class Ext  // Extension Methods
+    {
+        public static int Word_Count(this string s, string words)  // Existing Method Extension
+        {
+            int count = 1;
+
+            string[] wordList = words.Split(' ');
+            count = wordList.Length;
+
+            foreach (string word in wordList) { Console.WriteLine(word); }
+            Console.WriteLine();
+
+            /*foreach (char c in words)
+            {
+                if (c == ' ')
+                    count++;
+            }*/
+
+            return count;
+        }
+
+        public static void LogError(this Exception ex1, string errname)  // Custom Exception Extension
+        {
+            FileStream fs = new FileStream(@"ErrorLog.txt", FileMode.Append);
+            StreamWriter str = new StreamWriter(fs);
+
+            str.WriteLine("Error Occured in {0} at {1}", errname, System.DateTime.Now.ToString());
+            str.WriteLine(ex1.Message);
+            str.WriteLine();
+
+            str.Close();
+            fs.Close();
+
+            Console.WriteLine(errname);
+            Console.WriteLine(ex1.Message);
+        }
+    }
+
+    public delegate void dlg(int n1, int n2);
+
     internal class Program
     {
         static void ReadData(ArrayList ar) // Generic Class - Strongly Typed Class
@@ -21,9 +64,61 @@ namespace _25June
             DataStore<int> dlist = new DataStore<int>();
             List<string> slist = new List<string>();
         }
+
+        static void add(int n1, int n2) { Console.WriteLine(n1 + n2); }
+        static void subtract(int n1, int n2) { Console.WriteLine(n1 - n2); }
+
+
+        public static void Event(dlg d) 
+        {
+            dlg d1 = new dlg(add);
+            dlg d2 = new dlg(subtract);
+            d1.Invoke(6, 5);
+            d2.Invoke(6, 5);
+
+            d(200, 100);
+        }
+
         static void Main(string[] args)
         {
+            dlg d0 = new dlg (add);
+            d0 += new dlg(subtract);
+            Event(d0);
 
+            //string words = "Hi, I am Vishrut Aryan!";
+            //Console.WriteLine(words.Word_Count(words));
+
+            /*try
+            {
+                Queue<int> qlist1 = new Queue<int>();
+                qlist1.Enqueue(1);
+                qlist1.Enqueue(2);
+                qlist1.Enqueue(3);
+                qlist1.Enqueue(4);
+                qlist1.Enqueue(5);
+                List<int> qlist2 = qlist1.ToList();
+
+
+                Queue qlist = new Queue();
+                qlist.Enqueue("Vishrut");
+                qlist.Enqueue(280);
+
+                foreach (object o in qlist)
+                {
+                    System.Console.WriteLine(o);
+                }
+                Console.WriteLine();
+
+                object o1 = qlist.Dequeue();
+                Console.WriteLine(o1);
+                Console.WriteLine();
+
+                throw new Exception("We trying to do a thing");
+            }
+            catch (Exception ex) 
+            {
+                ex.LogError("HOLTEC-APP");
+            }*/
 
             /*Stack:
             Stack s1 = new Stack();
