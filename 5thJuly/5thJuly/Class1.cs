6d794cae6e41006a08368730b803263a7ce537e1
Vishrut Aryan <VAryan@holtecasia.com>
2024-07-05 13:52:57 +0530

    we didsome ado, but not Ado, we do ADO.... and some .NET

---
+++ 5thJuly/5thJuly/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5thJuly
+{
+    internal class Employee
+    {
+        public int Id;
+        public string EmpName;
+        public double Salary;
+
+        public void disconnDB(SqlConnection conn1)
+        {
+            SqlDataAdapter adap = new SqlDataAdapter("SELECT * FROM emp1", conn1);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);  //  Opens the connection, Grabs the data, Auto closes the connection
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.Write(dr[0].ToString() + " ");
+                Console.Write(dr[1].ToString() + " ");
+                Console.Write(dr[2].ToString() + " ");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void connDB(SqlConnection conn1)
+        {
+            List<Employee> emplist = new List<Employee>();
+            SqlDataReader adr = null;
+            conn1.Open();  //  We open the connection
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM emp1", conn1);
+            adr = cmd.ExecuteReader();
+
+            while (adr.Read())  //  We grab the data
+            {
+                Employee e1 = new Employee();
+                e1.Id = Convert.ToInt32(adr[0]);
+                e1.EmpName = adr[1].ToString();
+                e1.Salary = Convert.ToDouble(adr[2]);
+                emplist.Add(e1);
+            }
+            conn1.Close();  //  We close the connection
+
+            foreach (Employee e in emplist)
+            {
+                Console.Write(e.Id + " ");
+                Console.Write(e.EmpName + " ");
+                Console.Write(e.Salary + " ");
+                Console.WriteLine("");
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
---
+++ 5thJuly2/5thJuly2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5thJuly2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SqlConnection coon2 = new SqlConnection("Data Source = HA-NB69\\SQLEXPRESS;Initial Catalog = AdventureWorksDW2016;User Id = sa;Password = 12345;");
+            SqlTransaction tran1;
+
+            coon2.Open();
+            tran1 = coon2.BeginTransaction();
+
+            try
+            {
+                SqlCommand comm1 = new SqlCommand("INSERT INTO CUSTOMER VALUES (1, 'AMIT', 'PUNE')", coon2, tran1);
+                SqlCommand comm2 = new SqlCommand("INSERT INTO CUSTOMER VALUES (2, 'SUNIL', 'PUNE')", coon2, tran1);
+                SqlCommand comm3 = new SqlCommand("INSERT INTO CUSTOMER VALUES (1, 'KAPIL', 'PUNE')", coon2, tran1);
+                SqlCommand comm4 = new SqlCommand("INSERT INTO CUSTOMER VALUES (4, 'RANJIT', 'PUNE')", coon2, tran1);
+
+                comm1.ExecuteNonQuery();
+                comm2.ExecuteNonQuery();
+                comm3.ExecuteNonQuery();
+                comm4.ExecuteNonQuery();
+
+                tran1.Commit();
+                Console.WriteLine("Transactions Commited");
+            }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    tran1.Rollback();
+                }
+                catch ( Exception ex1) {  Console.WriteLine(ex1.Message); }
+            }
+            coon2.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
